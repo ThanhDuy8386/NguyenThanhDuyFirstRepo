6ddba1306ca2a32e98a1b81a39275392fe22321f
eiuThanhDuy <duynguyen120504@gmail.com>
2024-11-29 13:45:35 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("duy");
+        System.out.println("hello dduy");
+        System.out.println("welcome to my repo");
     }
 }
